115bd9bef055a02bd6c7629e272d35c07f1baedf
darthmorgot <dartmorgot@yandex.ru>
2022-04-02 02:48:21 +1000

    Добавил класс для отображения страницы контактов.

--- information/views.py
+++ information/views.py
@@ -9,3 +9,13 @@ class AboutPageView(TemplateView):
         context['title'] = 'О нас'
         context['cart_range'] = range(1, 3)
         return context
+
+
+class ContactPageView(TemplateView):
+    template_name = 'information/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Контакты'
+        context['cart_range'] = range(1, 3)
+        return context
