e1977578f5017c33f7bd4cef921c871c20b4fcf0
darthmorgot <dartmorgot@yandex.ru>
2022-04-02 02:55:51 +1000

    Добавил класс для отображения страницы быстрого просмотра.

--- products/views.py
+++ products/views.py
@@ -18,3 +18,11 @@ class HomePageView(TemplateView):
         context['product_range'] = range(1, 7)
         context['cart_range'] = range(1, 3)
         return context
+
+
+class PopupPageView(TemplateView):
+    template_name = 'products/ajax/product_quick_view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
