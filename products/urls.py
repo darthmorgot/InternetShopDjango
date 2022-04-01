8116eb44cf2f89bcb2a0f5acd5c679db9823401c
darthmorgot <dartmorgot@yandex.ru>
2022-04-02 02:54:31 +1000

    Добавил в файл путь для страницы быстрого просмотра.

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, PopupPageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('popup/', PopupPageView.as_view(), name='popup'),
 ]
