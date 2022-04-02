44c6894d7759da3c1399e3edc987786a3c739c54
darthmorgot <dartmorgot@yandex.ru>
2022-04-03 01:51:12 +1000

    Добавил путь для представления каталога.

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView, PopupPageView
+from .views import HomePageView, PopupPageView, CatalogPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('popup/', PopupPageView.as_view(), name='popup'),
+    path('catalog/', CatalogPageView.as_view(), name='catalog'),
 ]
