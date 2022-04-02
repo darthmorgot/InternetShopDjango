4346436863c50d45f220ab53c6c9d81a009ff58a
darthmorgot <dartmorgot@yandex.ru>
2022-04-03 01:52:05 +1000

    Добавил класс для представления каталога.

--- products/views.py
+++ products/views.py
@@ -26,3 +26,14 @@ class PopupPageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+class CatalogPageView(TemplateView):
+    template_name = 'products/catalog.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Каталог'
+        context['product_range'] = range(1, 11)
+        context['cart_range'] = range(1, 3)
+        return context
