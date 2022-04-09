36671c5bffed15ffaa02e86a57631a171fc969b9
darthmorgot <dartmorgot@yandex.ru>
2022-04-09 23:25:30 +1000

    Добавил путь для отображения страницы в разработке.

--- information/urls.py
+++ information/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import AboutPageView, ContactPageView
+from .views import AboutPageView, ContactPageView, PageDevelopmentView
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
     path('contact/', ContactPageView.as_view(), name='contact'),
+    path('page-development/', PageDevelopmentView.as_view(), name='page_development'),
 ]
