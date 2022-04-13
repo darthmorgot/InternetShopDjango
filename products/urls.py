f8c6508b5f127d135508adaece565d83870a5b4b
darthmorgot <dartmorgot@yandex.ru>
2022-04-13 17:58:19 +1000

    Добавил путь для страницы продукта.

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import HomePageView, ProductQuickViewView, CatalogPageView
+from .views import HomePageView, ProductQuickViewView, CatalogPageView, ProductPageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
     path('quick-view/', ProductQuickViewView.as_view(), name='quick_view'),
+    path('product/', ProductPageView.as_view(), name='product'),
     path('catalog/', CatalogPageView.as_view(), name='catalog'),
+    path('', HomePageView.as_view(), name='home'),
 ]
+
