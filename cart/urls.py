930e9854beda5245972c5cd0b22c73f3d7feb7a2
darthmorgot <dartmorgot@yandex.ru>
2022-04-14 23:43:50 +1000

    Добавил путь для отображения страницы оформления заказа.

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CartPageView
+from .views import CartPageView, ShippingPageView
 
 urlpatterns = [
     path('cart/', CartPageView.as_view(), name='cart'),
+    path('shipping/', ShippingPageView.as_view(), name='shipping'),
 ]
