408640f1cb3abb164b3679c42c7370692756ba6b
darthmorgot <dartmorgot@yandex.ru>
2022-04-20 00:33:31 +1000

    Добавил пути для вновь созданных страниц.

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import DashboardPageView
+from .views import DashboardPageView, ForgotPasswordPageView, LoginPageView, AccountPageView
 
 urlpatterns = [
     path('dashboard/', DashboardPageView.as_view(), name='dashboard'),
+    path('forgot-password/', ForgotPasswordPageView.as_view(), name='forgot_password'),
+    path('login/', LoginPageView.as_view(), name='login'),
+    path('account/', AccountPageView.as_view(), name='account'),
 ]
