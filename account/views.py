8af0ab6c65889139c8bebd39425bb0703479ee25
darthmorgot <dartmorgot@yandex.ru>
2022-04-20 00:33:59 +1000

    Добавил классы для вновь созданных страниц.

--- account/views.py
+++ account/views.py
@@ -9,3 +9,33 @@ class DashboardPageView(TemplateView):
         context['title'] = 'Личный кабинет'
         context['cart_range'] = range(1, 3)
         return context
+
+
+class ForgotPasswordPageView(TemplateView):
+    template_name = 'account/forgot_password.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Сброс пароля'
+        context['cart_range'] = range(1, 3)
+        return context
+
+
+class LoginPageView(TemplateView):
+    template_name = 'account/login.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Вход'
+        context['cart_range'] = range(1, 3)
+        return context
+
+
+class AccountPageView(TemplateView):
+    template_name = 'account/account.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Учетная запись'
+        context['cart_range'] = range(1, 3)
+        return context
