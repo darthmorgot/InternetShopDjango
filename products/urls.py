6d7d20cb41c51e200d12702c65071b6f869c179e
darthmorgot <dartmorgot@yandex.ru>
2022-04-22 00:19:18 +1000

    Удалил путь для главной страницы в связи с переносом.

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import HomePageView, ProductQuickViewView, CatalogPageView, ProductPageView
+from .views import ProductQuickViewView, CatalogPageView, ProductPageView
 
 urlpatterns = [
     path('quick-view/', ProductQuickViewView.as_view(), name='quick_view'),
     path('product/', ProductPageView.as_view(), name='product'),
     path('catalog/', CatalogPageView.as_view(), name='catalog'),
-    path('', HomePageView.as_view(), name='home'),
 ]
 
