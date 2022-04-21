809940acf1211b2d7c99ed318be429be70eb4898
darthmorgot <dartmorgot@yandex.ru>
2022-04-22 00:18:07 +1000

    Добавил класс для главной страницы.

--- information/views.py
+++ information/views.py
@@ -1,6 +1,17 @@
 from django.views.generic import TemplateView
 
 
+class HomePageView(TemplateView):
+    template_name = 'information/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная'
+        context['product_range'] = range(1, 7)
+        context['cart_range'] = range(1, 3)
+        return context
+
+
 class AboutPageView(TemplateView):
     template_name = 'information/about.html'
 
