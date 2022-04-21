11791ca79308e862b5c96e8f5ecae260b46031cf
darthmorgot <dartmorgot@yandex.ru>
2022-04-22 00:21:35 +1000

    Удалил класс для главной страницы в связи с переносом.

--- products/views.py
+++ products/views.py
@@ -1,23 +1,4 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
-
-
-# def index(request):
-#     context = {
-#         'title': 'Главная',
-#     }
-#     return render(request, 'products/index.html', context=context)
-
-
-class HomePageView(TemplateView):
-    template_name = 'products/index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная'
-        context['product_range'] = range(1, 7)
-        context['cart_range'] = range(1, 3)
-        return context
 
 
 class ProductQuickViewView(TemplateView):
