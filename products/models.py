4ec91c25bad1c350f13347e6f76cad465a3efa73
darthmorgot <dartmorgot@yandex.ru>
2022-04-27 00:25:08 +1000

    Создал классы для моделей Product и Category.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='product_images', verbose_name='Картинка')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('product', kwargs={'product_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Товары'
+        verbose_name_plural = 'Товары'
+        ordering = ['id']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Название')
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('catalog', kwargs={'cat_slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Категории'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
