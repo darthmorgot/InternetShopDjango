47eece95a0c747fd7b8c645c4f037b8d64f0c9f3
darthmorgot <dartmorgot@yandex.ru>
2022-04-30 01:33:05 +1000

    Добавил класс в связи с добавлением модуля django-mptt.

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
 from .models import Product, Category
 
+
+class CustomMPTTModelAdmin(MPTTModelAdmin):
+    mptt_level_indent = 20
+
+
 admin.site.register(Product)
-admin.site.register(Category)
+admin.site.register(Category, CustomMPTTModelAdmin)
