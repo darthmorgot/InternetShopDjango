a63022f78851c8afc0134701ff0c3bb0bc17a0c3
darthmorgot <dartmorgot@yandex.ru>
2022-04-30 01:34:33 +1000

    Изменил код файла в связи с добавлением модуля django-mptt для создания древовидной структуры каталога.

--- products/models.py
+++ products/models.py
@@ -1,6 +1,29 @@
 from django.db import models
 from django.urls import reverse
 
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Название')
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL')
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children',
+                            verbose_name='Родительская')
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('catalog', kwargs={'category_slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Категории'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name='Название')
@@ -8,7 +31,8 @@ class Product(models.Model):
     image = models.ImageField(upload_to='product_images', verbose_name='Картинка')
     price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена')
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='products',
+                                 verbose_name='Категория')
 
     def __str__(self):
         return self.name
@@ -19,20 +43,4 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Товары'
         verbose_name_plural = 'Товары'
-        ordering = ['id']
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True, verbose_name='Название')
-    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL')
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('catalog', kwargs={'cat_slug': self.slug})
-
-    class Meta:
-        verbose_name = 'Категории'
-        verbose_name_plural = 'Категории'
         ordering = ['id']
