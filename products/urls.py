79e9fb1dfa05eaa2fbe81c0a14d614f2d94536c1
darthmorgot <dartmorgot@yandex.ru>
2022-05-01 02:23:41 +1000

    Добавил путь для страницы с товарами из указанной категории.

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import ProductQuickViewView, CatalogPageView, ProductPageView
+from .views import ProductQuickViewView, CatalogPageView, ProductPageView, test
 
 urlpatterns = [
     path('quick-view/', ProductQuickViewView.as_view(), name='quick_view'),
     path('product/', ProductPageView.as_view(), name='product'),
     path('catalog/', CatalogPageView.as_view(), name='catalog'),
+    path('catalog/<slug:category_slug>', test, name='product_by_category'),
 ]
-
