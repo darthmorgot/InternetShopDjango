550637c5f5a21e1b7ebb7e11df716519826e7038
darthmorgot <dartmorgot@yandex.ru>
2022-05-01 02:20:20 +1000

    Изменил код, чтобы в админке была возможность перетаскивать категории, и менять структуру каталога.

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from mptt.admin import MPTTModelAdmin
+from mptt.admin import DraggableMPTTAdmin
 
 from .models import Product, Category
 
 
-class CustomMPTTModelAdmin(MPTTModelAdmin):
+class CustomMPTTModelAdmin(DraggableMPTTAdmin):
     mptt_level_indent = 20
+    list_display = ['tree_actions', 'indented_title']
+    list_display_links = ['indented_title']
 
 
 admin.site.register(Product)
