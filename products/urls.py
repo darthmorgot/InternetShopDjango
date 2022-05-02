56a975273fa9c49dfbf4fedbdf1d12ff0acbd330
darthmorgot <dartmorgot@yandex.ru>
2022-05-03 01:02:12 +1000

    Поправил код, вместо функции test будет класс CatalogPageView.

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import ProductQuickViewView, CatalogPageView, ProductPageView, test
+from .views import ProductQuickViewView, CatalogPageView, ProductPageView
 
 urlpatterns = [
     path('quick-view/', ProductQuickViewView.as_view(), name='quick_view'),
     path('product/', ProductPageView.as_view(), name='product'),
     path('catalog/', CatalogPageView.as_view(), name='catalog'),
-    path('catalog/<slug:category_slug>', test, name='product_by_category'),
+    path('catalog/<slug:category_slug>', CatalogPageView.as_view(), name='product_by_category'),
 ]
