6cfede9bb28f6a040d64342ba0ebc9c9ed1cfe89
darthmorgot <dartmorgot@yandex.ru>
2022-05-03 00:59:33 +1000

    Раскомментировал функцию в классе Category, добавил поле в классе Product, добавил класс для модели Image.

--- products/models.py
+++ products/models.py
@@ -13,8 +13,8 @@ class Category(MPTTModel):
     def __str__(self):
         return self.name
 
-    # def get_absolute_url(self):
-    #     return reverse('product_by_category', kwargs={'category_slug': self.slug})
+    def get_absolute_url(self):
+        return reverse('product_by_category', kwargs={'category_slug': self.slug})
 
     class Meta:
         verbose_name = 'Категории'
@@ -27,11 +27,11 @@ class Category(MPTTModel):
 
 class Product(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name='Название')
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL', null=True)
     description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(upload_to='product_images', verbose_name='Картинка')
-    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена')
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='products',
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products',
                                  verbose_name='Категория')
 
     def __str__(self):
@@ -43,4 +43,18 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Товары'
         verbose_name_plural = 'Товары'
+        ordering = ['id']
+
+
+def upload_gallery_image(instance, filename):
+    return f'product_images/{instance.product.slug}/{filename}'
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to=upload_gallery_image, verbose_name='Картинка')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images', verbose_name='Товар')
+
+    class Meta:
+        verbose_name = 'Галерея'
+        verbose_name_plural = 'Галерея'
         ordering = ['id']
