9927cc67bb8100e450e3fa620fc549c58c4c7ea0
darthmorgot <dartmorgot@yandex.ru>
2022-05-10 21:30:29 +1000

    Добавил поле для скидки в модель Product, также функцию upload_gallery_image, upload_product_image, которые устанавливают путь, где будут храниться картинки. Добавил функцию validate_range для валидации значения скидки, также функцию get_discount_price для вычисления цены со скидкой. Изменил тип поля для price, и еще небольшие правки кода.

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.core.exceptions import ValidationError
 
 from mptt.models import MPTTModel, TreeForeignKey
 
@@ -25,33 +26,43 @@ class Category(MPTTModel):
         order_insertion_by = ['name']
 
 
-def upload_gallery_image(instance, filename):
-    if instance.product:
-        return f'product_images/{instance.product.slug}/{filename}'
-    else:
-        return f'product_images/{instance.slug}/{filename}'
+def upload_product_image(instance, filename):
+    return f'product_images/{instance.slug}/{filename}'
+
+
+def validate_range(value):
+    if value > 100:
+        raise ValidationError('Размер скидки не может быть больше 100%')
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name='Название')
     slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL', null=True)
     description = models.TextField(verbose_name='Описание')
-    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
+    price = models.PositiveIntegerField(default=0, verbose_name='Цена')
+    discount = models.PositiveIntegerField(default=0, validators=[validate_range], verbose_name='Скидка, %')
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products',
                                  verbose_name='Категория')
-    image = models.ImageField(upload_to=upload_gallery_image, verbose_name='Картинка', null=True)
+    image = models.ImageField(upload_to=upload_product_image, verbose_name='Картинка', null=True)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('product', kwargs={'product_id': self.pk})
+        return reverse('product_test', kwargs={'product_id': self.pk})
+
+    def get_discount_price(self):
+        return int(self.price - (self.price * self.discount / 100))
 
     class Meta:
-        verbose_name = 'Товары'
-        verbose_name_plural = 'Товары'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
         ordering = ['id']
+
+
+def upload_gallery_image(instance, filename):
+    return f'product_images/{instance.product.slug}/{filename}'
 
 
 class Image(models.Model):
