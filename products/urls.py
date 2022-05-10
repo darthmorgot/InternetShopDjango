b09f7c933eda0a954e1c4c321b1a52cb565595be
darthmorgot <dartmorgot@yandex.ru>
2022-05-10 21:31:59 +1000

    Добавил путь для страницы товара с параметром id.

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,7 @@ from .views import ProductQuickViewView, CatalogPageView, ProductPageView
 urlpatterns = [
     path('quick-view/', ProductQuickViewView.as_view(), name='quick_view'),
     path('product/', ProductPageView.as_view(), name='product'),
+    path('product/<int:product_id>', ProductPageView.as_view(), name='product_test'),
     path('catalog/', CatalogPageView.as_view(), name='catalog'),
     path('catalog/<slug:category_slug>', CatalogPageView.as_view(), name='product_by_category'),
 ]
