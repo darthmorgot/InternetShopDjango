5352e3d0907ce993ab4bd974ff7cdc6a953f6195
darthmorgot <dartmorgot@yandex.ru>
2022-05-13 20:25:42 +1000

    Изменил код в классах для отображения страниц каталога и продукта, удалил переменную categories.

--- products/views.py
+++ products/views.py
@@ -1,8 +1,6 @@
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import DetailView, ListView
 
 from products.models import Category, Product, Image
-
-categories = Category.objects.all()
 
 
 class ProductQuickViewView(DetailView):
@@ -16,25 +14,32 @@ class ProductQuickViewView(DetailView):
         return context
 
 
-class CatalogPageView(TemplateView):
+class CatalogPageView(ListView):
+    model = Product
     template_name = 'products/catalog.html'
 
     def get_context_data(self, **kwargs):
+        products = Product.objects.all()
         context = super().get_context_data(**kwargs)
         context['title'] = 'Каталог'
-        context['product_range'] = range(1, 11)
         context['cart_range'] = range(1, 3)
-        context['categories'] = categories
+        context['categories'] = Category.objects.all()
+        context['products'] = products
+        context['number_products'] = products.count()
         return context
 
 
-class ProductPageView(TemplateView):
+class ProductPageView(DetailView):
+    model = Product
     template_name = 'products/product.html'
+    pk_url_kwarg = 'product_id'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Товар'
-        context['product_range'] = range(1, 7)
         context['cart_range'] = range(1, 3)
-        context['categories'] = categories
+        context['categories'] = Category.objects.all()
+        context['product'] = Product.objects.get(pk=self.kwargs['product_id'])
+        context['products'] = Product.objects.all()[:6]
+        context['images'] = Image.objects.filter(product__pk=self.kwargs['product_id'])
         return context
