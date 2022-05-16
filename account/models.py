6a92bc98ec5b8cf4be075274eaca4a2c5c82b5de
darthmorgot <dartmorgot@yandex.ru>
2022-05-16 22:06:23 +1000

    Создал модель User для пользователей.

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=150, null=True, blank=True)
+    email = models.EmailField(unique=True, verbose_name='E-mail')
+    phone = models.CharField(max_length=11, null=True, blank=True, verbose_name='Телефон')
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+
+# class Address(models.Model):
+#     pass
