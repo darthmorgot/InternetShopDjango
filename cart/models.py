1a07ff949581374c6097536d4dae3c5bf2009a2b
darthmorgot <dartmorgot@yandex.ru>
2022-05-16 22:07:53 +1000

    Начал создание модели корзины.

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# from account.models import User
+from products.models import Product
+
+
+class Cart(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='Пользователь')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, verbose_name='Продукт')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    date_creation = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return f'Корзина для user | Продукт {self.product.name}'
