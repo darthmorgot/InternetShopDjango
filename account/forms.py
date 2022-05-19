e809cb96be718dd203f0b74faf1454365ab69567
darthmorgot <dartmorgot@yandex.ru>
2022-05-19 21:50:50 +1000

    Добавил в класс поля email, password.

--- account/forms.py
+++ account/forms.py
@@ -1,9 +1,18 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
 from account.models import User
 
 
 class UserLoginForm(AuthenticationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'E-mail'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Пароль'
+    }))
 
     class Meta:
         model = User
