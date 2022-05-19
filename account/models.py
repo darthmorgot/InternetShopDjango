20e26b91645a6be9680e2c27b7b8256ed3f5bd3d
darthmorgot <dartmorgot@yandex.ru>
2022-05-19 21:53:20 +1000

    Изменил модель пользователя, добавил класс UserManager для переопределения настроек по умолчанию.

--- account/models.py
+++ account/models.py
@@ -1,13 +1,42 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **other_fields):
+        if not email:
+            raise ValueError('Необходимо указать ваш email')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **other_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Для суперпользователя установите is_staff=True')
+
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Для суперпользователя установите is_superuser=True')
+
+        return self.create_user(email, password, **other_fields)
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=150, null=True, blank=True)
+    username = None
     email = models.EmailField(unique=True, verbose_name='E-mail')
-    phone = models.CharField(max_length=11, null=True, blank=True, verbose_name='Телефон')
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
 
 # class Address(models.Model):
