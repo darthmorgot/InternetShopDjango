ba434329582ff3722e2fd8e5e742d3600f24bfa5
darthmorgot <dartmorgot@yandex.ru>
2022-05-19 22:00:23 +1000

    Временно закомментировал содержимое класса корзины.

--- cart/models.py
+++ cart/models.py
@@ -5,10 +5,11 @@ from products.models import Product
 
 
 class Cart(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='Пользователь')
-    product = models.ForeignKey(Product, on_delete=models.PROTECT, verbose_name='Продукт')
-    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
-    date_creation = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-
-    def __str__(self):
-        return f'Корзина для user | Продукт {self.product.name}'
+    pass
+#     # user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='Пользователь')
+#     product = models.ForeignKey(Product, on_delete=models.PROTECT, verbose_name='Продукт')
+#     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+#     date_creation = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+#
+#     def __str__(self):
+#         return f'Корзина для user | Продукт {self.product.name}'
