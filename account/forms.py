eed004c56db48760a2b414a7448251ec5eaa9ac8
darthmorgot <dartmorgot@yandex.ru>
2022-05-21 22:14:34 +1000

    Добавил модель для формы регистрации, и мелки правки раннего кода.

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from account.models import User
 
@@ -7,13 +7,40 @@ from account.models import User
 class UserLoginForm(AuthenticationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={
         'class': 'form-control',
-        'placeholder': 'E-mail'
+        'placeholder': 'Email',
     }))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'form-control',
-        'placeholder': 'Пароль'
+        'placeholder': 'Пароль',
     }))
 
     class Meta:
         model = User
         fields = ['email', 'password']
+
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Имя',
+    }))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Фамилия',
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Email',
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Пароль',
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Подтвердите пароль',
+    }))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
