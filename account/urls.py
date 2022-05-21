a268c11a69ad1798d470e9f257781795c4f6c8d1
darthmorgot <dartmorgot@yandex.ru>
2022-05-21 22:17:06 +1000

    Добавил пути для регистрации пользователя и выхода из системы.

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import DashboardPageView, ForgotPasswordPageView, LoginPageView, AccountPageView
+from .views import DashboardPageView, ForgotPasswordPageView, LoginPageView, UserRegistration, AccountPageView, \
+    UserLogout
 
 urlpatterns = [
     path('dashboard/', DashboardPageView.as_view(), name='dashboard'),
     path('forgot-password/', ForgotPasswordPageView.as_view(), name='forgot_password'),
-    path('login/', LoginPageView.as_view(), name='login'),
+    # path('login/', LoginPageView.as_view(), name='login'),
+    path('login/', UserRegistration.as_view(), name='login'),
+    path('logout/', UserLogout.as_view(), name='logout'),
     path('account/', AccountPageView.as_view(), name='account'),
 ]
