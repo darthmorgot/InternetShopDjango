ef32d5bddafe675f73ca2b267e470cffff4031bb
darthmorgot <dartmorgot@yandex.ru>
2022-06-03 23:22:22 +1000

    Удалил класс UserManager, и поправил код класса User.

--- account/models.py
+++ account/models.py
@@ -2,37 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
-class UserManager(BaseUserManager):
-    def create_user(self, email, password, **other_fields):
-        if not email:
-            raise ValueError('Необходимо указать ваш email')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **other_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password, **other_fields):
-        other_fields.setdefault('is_staff', True)
-        other_fields.setdefault('is_superuser', True)
-        other_fields.setdefault('is_active', True)
-
-        if other_fields.get('is_staff') is not True:
-            raise ValueError('Для суперпользователя установите is_staff=True')
-
-        if other_fields.get('is_superuser') is not True:
-            raise ValueError('Для суперпользователя установите is_superuser=True')
-
-        return self.create_user(email, password, **other_fields)
-
-
 class User(AbstractUser):
-    username = None
+    username = models.CharField(max_length=50, null=True, blank=True)
     email = models.EmailField(unique=True, verbose_name='E-mail')
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-
-    objects = UserManager()
 
     class Meta:
         verbose_name = 'Пользователь'
