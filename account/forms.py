54b7694384de3e3dbc40b35db04de229298fb6c7
darthmorgot <dartmorgot@yandex.ru>
2022-06-03 23:19:23 +1000

    Небольшие правки кода классов форм.

--- account/forms.py
+++ account/forms.py
@@ -5,42 +5,38 @@ from account.models import User
 
 
 class UserLoginForm(AuthenticationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
-        'class': 'form-control',
+    username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Email',
     }))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control',
         'placeholder': 'Пароль',
     }))
 
-    class Meta:
-        model = User
-        fields = ['email', 'password']
+    def __init__(self, request=None, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
 
 
 class UserRegistrationForm(UserCreationForm):
-    first_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control',
-        'placeholder': 'Имя',
-    }))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control',
-        'placeholder': 'Фамилия',
-    }))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
-        'class': 'form-control',
-        'placeholder': 'Email',
-    }))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control',
         'placeholder': 'Пароль',
     }))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control',
         'placeholder': 'Подтвердите пароль',
     }))
 
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': 'Имя'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Фамилия'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
+        }
+
+    def __init__(self, request=None, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
