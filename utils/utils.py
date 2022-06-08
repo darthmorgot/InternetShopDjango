1cc840c7d00ca925c274682af5789396209bf496
darthmorgot <dartmorgot@yandex.ru>
2022-06-09 00:40:36 +1000

    Добавил функцию для контекста, передаваемого в шаблоны страниц.

--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,12 @@ from products.models import Category
 
 class DataMixin:
     def get_user_context(self, **kwargs):
-        context = kwargs
-        context['cart_range'] = range(1, 3)
-        context['categories'] = Category.objects.all()
+        context = get_context(**kwargs)
         return context
+
+
+def get_context(**kwargs):
+    context = kwargs
+    context['cart_range'] = range(1, 3)
+    context['categories'] = Category.objects.all()
+    return context
