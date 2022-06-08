da36c726ca5863f9e53e1f99b37533f98fb5c3e4
darthmorgot <dartmorgot@yandex.ru>
2022-06-09 00:34:05 +1000

    Добавил путь для страницы входа-регистрации, поправил имена путей.

--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from .views import DashboardPageView, ForgotPasswordPageView, LoginPageView, UserRegistration, AccountPageView, \
-    UserLogout
+from .views import DashboardPageView, AccountPageView, ForgotPasswordPageView, LoginPageView, UserRegistration, \
+    UserLogout, login_register_user
 
 urlpatterns = [
     path('dashboard/', DashboardPageView.as_view(), name='dashboard'),
-    path('forgot-password/', ForgotPasswordPageView.as_view(), name='forgot_password'),
-    # path('login/', LoginPageView.as_view(), name='login'),
-    path('login/', UserRegistration.as_view(), name='login'),
-    path('logout/', UserLogout.as_view(), name='logout'),
     path('account/', AccountPageView.as_view(), name='account'),
+    path('forgot-password/', ForgotPasswordPageView.as_view(), name='forgot_password'),
+    path('login/', LoginPageView.as_view(), name='login_form'),
+    path('register/', UserRegistration.as_view(), name='register_form'),
+    path('logout/', UserLogout.as_view(), name='logout'),
+    path('login_register/', login_register_user, name='login_register'),
 ]
+
