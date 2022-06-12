a6e703dc8bc6626e4c353337b93026256c552117
darthmorgot <dartmorgot@yandex.ru>
2022-06-12 21:58:49 +1000

    Добавил путь для страницы каталога с выбранной категорией продуктов.

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import ProductQuickViewView, CatalogPageView, ProductPageView
+from .views import ProductQuickViewView, CatalogPageView, ProductPageView, CategoryPageView
 
 urlpatterns = [
     path('quick-view/<int:pk>', ProductQuickViewView.as_view(), name='quick_view'),
     path('product/<int:product_id>', ProductPageView.as_view(), name='product'),
+    path('catalog/<slug:category_slug>', CategoryPageView.as_view(), name='product_by_category'),
     path('catalog/', CatalogPageView.as_view(), name='catalog'),
-    path('catalog/<slug:category_slug>', CatalogPageView.as_view(), name='product_by_category'),
 ]
