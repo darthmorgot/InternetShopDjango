6440fa5971b217f61bcf757dce3837021913a293
darthmorgot <dartmorgot@yandex.ru>
2022-06-16 01:28:58 +1000

    Добавил пути для страниц с формами сброса пароля.

--- account/urls.py
+++ account/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
 
-from .views import DashboardPageView, AccountPageView, ForgotPasswordPageView, LoginPageView, UserRegistration, \
-    UserLogout, login_register_user
+from .views import \
+    DashboardPageView, AccountPageView, LoginPageView, UserRegistration, UserLogout, login_register_user, \
+    PasswordResetPageView, PasswordResetDonePageView, PasswordResetConfirmPageView, PasswordResetCompletePageView
+
 
 urlpatterns = [
     path('dashboard/', DashboardPageView.as_view(), name='dashboard'),
     path('account/', AccountPageView.as_view(), name='account'),
-    path('forgot-password/', ForgotPasswordPageView.as_view(), name='forgot_password'),
+    path('password_reset/', PasswordResetPageView.as_view(), name='password_reset_form'),
+    path('password_reset_done/', PasswordResetDonePageView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>', PasswordResetConfirmPageView.as_view(),
+         name='password_reset_confirm'),
+    path('password_reset_complete/', PasswordResetCompletePageView.as_view(), name='password_reset_complete'),
     path('login/', LoginPageView.as_view(), name='login_form'),
     path('register/', UserRegistration.as_view(), name='register_form'),
     path('logout/', UserLogout.as_view(), name='logout'),
     path('login_register/', login_register_user, name='login_register'),
 ]
-
